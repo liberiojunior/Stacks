c37a5af119c7599f378f225dd8b251b94f6093d9
Libério Júnior <108886389+liberiojunior@users.noreply.github.com>
2023-02-22 15:30:24 -0300

    First Commit

---
+++ Pratica_1/Pilha.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+ * CLASSE PILHA - IMPLEMENTA O TIPO ABSTRATO DE DADOS PILHA ESTÁTICA
+ * OPERAÇÕES: 
+ *	vazia() -> retorna true se a pilha está vazia.
+ *	cheia() -> returna true se a pilha está cheia.
+ *	empilhar(x) -> coloca o elemento x no topo da pilha.
+ *	desempilhar() -> retorna o elemento situado no topo da pilha.
+ */
+
+namespace Pratica_1
+{
+    class Pilha
+    {
+        private int[] vet; // vetor para armazenar os elementos da pilha
+        private int topo; // indica a posição do topo
+
+        public Pilha(int tam) // CONSTRUTOR
+        { 
+            vet = new int[tam];
+            topo = 0;
+        }
+
+        public bool cheia()
+        {
+            return (topo == vet.Length);
+        }
+
+        public bool vazia()
+        {
+            return (topo == 0);
+        }
+
+        public void empilhar(int x)
+        {
+            vet[topo] = x;
+            topo++;
+        }
+
+        public int desempilhar()
+        {
+            topo--;
+            return (vet[topo]);
+        }
+    }
+}
---
+++ Pratica_1/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pratica_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Calculadora Científica";
+            Console.BackgroundColor = ConsoleColor.Black;
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.Beep();
+            int aux = 0;
+            Pilha Pilha = new Pilha(100);
+            string msg = null;
+            while (msg != "s")
+            {
+
+                Console.WriteLine("Digite um número");
+
+                msg = Console.ReadLine();
+                if (msg == "s" || msg == "S") return;
+                if (msg == "+")
+                {
+                    while (Pilha.vazia()) { Console.WriteLine("Pilha vazia, favor digitar um número:"); msg = Console.ReadLine(); if (msg != "+") break; }
+                    int n1 = Convert.ToInt32(Pilha.desempilhar());
+                    while (Pilha.vazia()) { Console.WriteLine("Pilha vazia, favor digitar um número:"); msg = Console.ReadLine(); if (msg != "+") { aux = Convert.ToInt32(msg); Pilha.empilhar(aux); break; } }
+                    int n2 = Convert.ToInt32(Pilha.desempilhar());
+                    int temp = n1 + n2;
+                    Pilha.empilhar(temp);
+                    Console.WriteLine(temp);
+                }
+
+                else if (msg == "-")
+                {
+                    while (Pilha.vazia()) { Console.WriteLine("Pilha vazia, favor digitar um número:"); msg = Console.ReadLine(); if (msg != "-") break; }
+                    int n1 = Convert.ToInt32(Pilha.desempilhar());
+                    while (Pilha.vazia()) { Console.WriteLine("Pilha vazia, favor digitar um número:"); msg = Console.ReadLine(); if (msg != "-") { aux = Convert.ToInt32(msg); Pilha.empilhar(aux); break; } }
+                    int n2 = Convert.ToInt32(Pilha.desempilhar());
+                    int temp = n2 - n1;
+                    Pilha.empilhar(temp);
+                    Console.WriteLine(temp);
+                }
+                else if (msg == "*")
+                {
+                    while (Pilha.vazia()) { Console.WriteLine("Pilha vazia, favor digitar um número:"); msg = Console.ReadLine(); if (msg != "*") break; }
+                    int n1 = Convert.ToInt32(Pilha.desempilhar());
+                    while (Pilha.vazia()) { Console.WriteLine("Pilha vazia, favor digitar um número:"); msg = Console.ReadLine(); if (msg != "*") { aux = Convert.ToInt32(msg); Pilha.empilhar(aux); break; } }
+                    int n2 = Convert.ToInt32(Pilha.desempilhar());
+                    int temp = n1 * n2;
+                    Pilha.empilhar(temp);
+                    Console.WriteLine(temp);
+                }
+                else if (msg == "/")
+                {
+                    while (Pilha.vazia()) { Console.WriteLine("Pilha vazia, favor digitar um número:"); msg = Console.ReadLine(); if (msg != "/") break; }
+                    int n1 = Convert.ToInt32(Pilha.desempilhar());
+                    while (Pilha.vazia()) { Console.WriteLine("Pilha vazia, favor digitar um número:"); msg = Console.ReadLine(); if (msg != "/") { aux = Convert.ToInt32(msg); Pilha.empilhar(aux); break; } }
+                    int n2 = Convert.ToInt32(Pilha.desempilhar());
+                    int temp = n2 / n1;
+                    Pilha.empilhar(temp);
+                    Console.WriteLine(temp);
+                }
+                else Pilha.empilhar(Convert.ToInt32(msg));
+            }
+            Console.ReadKey();
+        }
+    }
+}
